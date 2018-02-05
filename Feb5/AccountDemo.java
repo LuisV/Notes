32c23ec16b7bab784b901ae80caec7765f4fba48
Luis <lvalencia@csumb.edu>
2018-02-05 12:41:21 -0800

    SOftware Design Feb5

---
+++ Feb5/AccountDemo.java
@@ -0,0 +1,44 @@
+public class Account {
+  private String acctName;
+  private int acctNumber;
+  private int acctType;
+  private double acctBalance;
+
+  public void setAccount(String aName, int aNumber, int aType, double aBalance) {
+    acctName = aName;
+    acctNumber = aNumber;
+    acctType = aType;
+    acctBalance = aBalance;
+  }
+
+  public void setAccount(String aName, int aNumber, double aBalance) {
+    acctName = aName;
+    acctNumber = aNumber;
+    acctType = 1;
+    acctBalance = aBalance;
+  }
+
+  public String getName(){
+    return (acctName);
+  }
+
+  public double getBalance() {
+    return (acctBalance);
+  }
+
+  public void deposit(double fund) {
+    if (fund < 0.0)  {
+      System.out.println("Error: no negative amount to deposit.");
+      System.exit(0);
+    }
+    acctBalance += fund;
+  }
+
+  public boolean withdrawal(double fund) {
+    // CST338 exercise
+  }
+
+  public String toString() {
+    return String.format("%s's account balance is $%,.2f", acctName, acctBalance);
+  }
+}
